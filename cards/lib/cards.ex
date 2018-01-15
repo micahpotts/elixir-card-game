ab12ad3580850a2d905488675fb9a02f6a811e1d
Micah Potts <mrs.micahpotts@gmail.com>
2018-01-14 23:00:35 -0500

    Save method and case statement for file load

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -21,4 +21,16 @@ defmodule Cards do
     Enum.split(deck, hand_size) 
   end
 
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
+  end
+
 end
